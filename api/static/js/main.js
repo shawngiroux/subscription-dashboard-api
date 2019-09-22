4a8b0adbc9f2df1e097db420bda0679c2f3b645a
Shawn Giroux <shawn.giroux90@gmail.com>
2019-09-21 23:17:16 -0400

    Adding minor form validation

--- api/static/js/main.js
+++ api/static/js/main.js
@@ -1,3 +1,13 @@
 function validateForm() {
-    // Form validation here
+    let password = document.getElementById('password').value.trim();
+    let confirm = document.getElementById('confirm').value.trim();
+
+    if (password === confirm) {
+        return true;
+    }
+
+    document.getElementById('confirm').setCustomValidity(
+        "Confirmation password does not match"
+    );
+    return false;
 }

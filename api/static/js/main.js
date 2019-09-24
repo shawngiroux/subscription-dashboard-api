15b46cbf4f362819201921ee17afec96bcfa9762
Shawn Giroux <shawn.giroux90@gmail.com>
2019-09-23 21:22:55 -0400

    Using fetch api instead of html forms

--- api/static/js/main.js
+++ api/static/js/main.js
@@ -1,3 +1,38 @@
+function submitForm(username, password) {
+    let url = "http://localhost:5000/AccountServices/register";
+    let body = {
+        username: username,
+        password: password
+    };
+    // TODO do something here to show user that form is being submitted
+    fetch(url, {
+        method: "POST",
+        headers: {
+            "Content-Type": "application/json"
+        },
+        body: JSON.stringify(body),
+        redirect: "follow"
+    })
+        .then((response) => {
+            if (response.status === 400) {
+                return response.json().then(err => Promise.reject(err));
+            }
+            if (response.redirected) {
+                window.location.href = response.url;
+            }
+            return response.json();
+        })
+        .then(() => {
+            // TODO handle body
+            let domain = document.domain;
+            window.location.href = domain + "/AccountServices/authenticate";
+        })
+        .catch((error) => {
+            // TODO handle error
+            console.log(error);
+        });
+}
+
 function validateForm() {
     let usernameEle = document.getElementById("username"),
         passwordEle = document.getElementById("password"),
